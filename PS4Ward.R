7461c71badd3513fca6ff7735dcc4a996436e139
warddalston <ward.dalston@gmail.com>
2014-02-15 11:45:43 -0600

    Directory Creation
    Wrote code that creates all of the appropriate directories.  Had to
    change the formatting of the time and date so that it would work with
    OSX.

---
+++ PS4Ward.R
@@ -0,0 +1,36 @@
+################################################
+### PS4 - Dalston Ward - February 20, 2014   ###
+################################################
+
+#Start everything by emptying the workspace.  (don't use this if concurrently working on other projects in R!)
+rm(list=ls())
+#Second, set the working directory as appropriate.  This makes the code cleaner and helps to keep track of any saved files.
+setwd("~/Documents/WashU 2nd Year/Applied Stats Programming/Feb 13/ProblemSet4/")
+#Third, set the seed for replicability
+set.seed(1801)
+
+############# Section A: Reading in data without a clean format #################
+
+### The goal is to create a generic function that reads in NetLogo data, and then outputs a directy with several .R,.csv, and .pdf files.  
+
+#First things first: make code that creates the directories
+DirName <- scan(file="NetLogo.csv", what="", sep=",", n=1, nlines=1, skip=1) #read in the name
+DirDate <- scan(file="NetLogo.csv", what="", sep=",", n=1, nlines=1, skip=2) #read in the date
+DirDate <- strsplit(DirDate," ") #Clean up the time a bit
+DirDate <- unlist(DirDate)[1:2] #drop the time zone part, keep the rest
+DirDate[1] <- gsub("/","-",DirDate[1]) #The slashes in the date confuse R when it tries to write a directory.  Use dashes instead. 
+DirDate[2] <- substr(DirDate[2],1,8) #drop the thousandths of seconds. 
+DirDate[2] <- gsub(":",".",DirDate[2]) #replace the colons with periods because OSX won't accept colons in a file name. 
+DirDate <- paste0(DirDate,collapse="_") #put the date and time back together.  
+DirCombined <- paste(DirName,DirDate,sep="_") #put name and date together
+if(!dir.create(DirCombined)) { #This loop creates the directory, and beacuse the ouput of dir.create is an invisible logical, allows for me to report a warning back to the user if it goes wrong.  
+  warning("Unable to create a directory!  Subsequent files might not be where you anticipate them.")
+}
+
+#Now, create the secondary directories using an sapply loop to do three at once. 
+sapply(c("Globals","Turtles","Plots"),function(x) dir.create(file.path(DirCombined,x)) ) #the file.path command links the parent directory to these sub directories.  
+
+#Finally, create the sub-sub directories in a similiar manner.
+sapply(c("PositionPlot","WinnersPlot","PolarizationPlot","IncumbentPercentagePlot"),function(x) dir.create(file.path(DirCombined,"Plots",x)) ) #the file.path command links the parent directory, and sub directory to these sub-sub directories.  
+
+
